d5b66ad80adec86ace257eb26972455d8129a951
Cachu <gcachu.o@gmail.com>
2016-11-06 16:45:21 -0600

    comando de ayuda

--- acciones.php
+++ acciones.php
@@ -9,7 +9,7 @@
 include "habitaciones.php";
 
 if (isset($_POST["accion"])) {
-    if (strpos($_POST["accion"], ' ') !== false) {
+    if (strpos($_POST["accion"], ' ') !== false or $_POST["accion"]=="ayuda") {
         $postAccion = explode(' ', $_POST["accion"]);
         $accion1 = $postAccion[0];
         $accion2 = $postAccion[1];
@@ -18,6 +18,9 @@ if (isset($_POST["accion"])) {
             case "ir":
                 $habitacion = siguienteHabitacion($_POST["jugador"], $accion2);
                 echo $habitacion["descripcion"];
+                break;
+            case "ayuda":
+                echo "Lista de comandos disponibles: <br>ir";
                 break;
         }
     } else
